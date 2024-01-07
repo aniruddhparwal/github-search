ab3587e936aefd864cbeb920636a655f2e24fd81
aniruddhparwal <aniruddhparwal@gmail.com>
2024-01-08 00:53:32 +0530

    Removed router-dom temp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Home/>
     </div>
   );
 }
